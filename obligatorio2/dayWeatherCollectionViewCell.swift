0a9adbf9dc447b9aa480170a721149efadfd7612
JuanPablo <juanpablo@JuanPablos-MacBook-Pro.local>
2017-06-03 13:57:05 -0300

    changes view

---
+++ obligatorio2/dayWeatherCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  dayWeatherCollectionViewCell.swift
+//  obligatorio2
+//
+//  Created by JuanPablo on 6/3/17.
+//  Copyright © 2017 UCUDAL. All rights reserved.
+//
+
+import UIKit
+
+class dayWeatherCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var weatherIconLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
+    
+}
