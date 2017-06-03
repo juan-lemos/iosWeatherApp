6ef023fcfb3321b47878ac9d9b2be443a427d4d9
JuanPablo <juanpablo@JuanPablos-MacBook-Pro.local>
2017-06-03 17:53:22 -0300

    maths

--- obligatorio2/MainViewController.swift
+++ obligatorio2/MainViewController.swift
@@ -17,41 +17,37 @@ class MainViewController:UIViewController{
     @IBOutlet weak var settingsButton: UIButton!
     @IBOutlet weak var weatherIconLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
-
     
-    var screenWidth:CGFloat?
-    var screenHeight:CGFloat?
     
+    let screenSize = UIScreen.main.bounds
+    
+    //MARK: -UIViewController methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        let screenSize = UIScreen.main.bounds
-        modifyConstraintsAndFontsSizes(screenSize:screenSize)
+        modifyConstraintsAndFontsSizes()
         
         print(cityLabel.font.pointSize)
         
         //print(temperatureLabel.size)
         
-//        print(temperatureLabel.font.pointSize)
-//        temperatureLabel.font = UIFont(name: "HelveticaNeue-UltraLight", size: 68.0)
-//        print(temperatureTopConstraint.constant)
-//        temperatureTopConstraint.constant=100
+        //        print(temperatureLabel.font.pointSize)
+        //        temperatureLabel.font = UIFont(name: "HelveticaNeue-UltraLight", size: 68.0)
+        //        print(temperatureTopConstraint.constant)
+        //        temperatureTopConstraint.constant=100
         
         // Do any additional setup after loading the view, typically from a nib.
-//        self.weatherIconLabel.text = WeatherIcon(condition: 201, iconString: "01n").iconText
+        //        self.weatherIconLabel.text = WeatherIcon(condition: 201, iconString: "01n").iconText
     }
-
     
-    //MARK: -utils methods
     
-    func modifyConstraintsAndFontsSizes( screenSize:CGRect){
+    //MARK: -Utils methods
+    func modifyConstraintsAndFontsSizes(){
         let originalWidth:CGFloat = 375.0
         let originalHeigth:CGFloat = 667.0
         let screenWidth = screenSize.width
         let screenHeight = screenSize.height
         let relationWidth = screenWidth/originalWidth
         let relationHeight = screenHeight/originalHeigth
-        
-        
         
         cityLabelTopConstraint.constant = cityLabelTopConstraint.constant * relationHeight
         buttonTopConstraint.constant = buttonTopConstraint.constant * relationHeight
@@ -61,11 +57,17 @@ class MainViewController:UIViewController{
         buttonHeightConstraint.constant = buttonHeightConstraint.constant * relationHeight
         collectionViewHeight.constant = collectionViewHeight.constant * relationHeight
         
-        cityLabel.font = UIFont(name: cityLabel.font.fontName , size: cityLabel.font.pointSize * relationHeight)
+        //relative to height because we are using always portrait
+        var multiplier:CGFloat = 1.0
+        if (screenHeight==480.0){//iphone4
+            multiplier = 1.2
+        }
+        cityLabel.font = UIFont(name: cityLabel.font.fontName , size: cityLabel.font.pointSize * relationHeight * multiplier)
         weatherIconLabel.font = UIFont(name: weatherIconLabel.font.fontName , size: weatherIconLabel.font.pointSize * relationHeight)
         temperatureLabel.font = UIFont(name: temperatureLabel.font.fontName , size: temperatureLabel.font.pointSize * relationHeight)
-        
     }
+    
+    
     
 }
 
